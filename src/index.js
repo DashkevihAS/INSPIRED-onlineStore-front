3fd8dc37181945168792220b1f08b6c29798eef9
DashkevichAS <antonsnov0604@gmail.com>
2023-01-17 11:51:15 +0300

    refactor render footer from api

--- src/index.js
+++ src/index.js
@@ -12,36 +12,51 @@ import { getData } from './modules/getData';
 
 import { API_URL, DATA } from './modules/const';
 import { createScssColors } from './modules/createScssColors';
+import { createElement } from './modules/createElement';
 
 const init = async () => {
-  DATA.navigation = await getData(`${API_URL}/api/categories`);
-  DATA.colors = await getData(`${API_URL}/api/colors`);
-  console.log('DATA.colors: ', DATA.colors);
-  createScssColors(DATA.colors);
+  try {
+    router.on('*', () => {
+      renderHeader();
+      renderFooter();
+    });
 
-  router.on('*', () => {
-    renderHeader();
-    renderFooter();
-  });
+    DATA.navigation = await getData(`${API_URL}/api/categories`);
+    DATA.colors = await getData(`${API_URL}/api/colors`);
+    createScssColors(DATA.colors);
 
-  router.on('/', () => {
-    mainPage();
-  });
-  router.on('women', () => {
-    womenMainPage();
-  });
-  router.on('men', () => {
-    menMainPage();
-  });
+    router.on('/', () => {
+      mainPage();
+    });
+    router.on('women', () => {
+      womenMainPage();
+    });
+    router.on('men', () => {
+      menMainPage();
+    });
 
-  // setTimeout(() => {
-  //   router.navigate('men');
-  // }, 3000);
-  // setTimeout(() => {
-  //   router.navigate('women');
-  // }, 6000);
-
-  router.resolve();
+    // setTimeout(() => {
+    //   router.navigate('men');
+    // }, 3000);
+    // setTimeout(() => {
+    //   router.navigate('women');
+    // }, 6000);
+  } catch (error) {
+    createElement(
+      'h2',
+      {
+        textContent: 'Данные не загрузились, попробуйте позже...',
+      },
+      {
+        parrent: document.querySelector('main'),
+        cb(h2) {
+          h2.style.textAlign = 'center';
+        },
+      },
+    );
+  } finally {
+    router.resolve();
+  }
 };
 
 init();
